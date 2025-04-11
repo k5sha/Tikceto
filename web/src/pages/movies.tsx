926b3b563b537455a6a2c5bd5ffd35d12faa0189
Yurii <yurri.yevtushenko.work@gmail.com>
2025-04-11 19:01:56 +0300

    test

--- web/src/pages/movies.tsx
+++ web/src/pages/movies.tsx
@@ -98,9 +98,10 @@ export default function MoviesPage() {
       <div className="max-w-7xl mx-auto px-4 py-6">
         {isAdmin && (
           <>
+              <h1>Test</h1>
             <h1 className="text-lg font-medium text-gray-900">Адмін панель</h1>
             <Divider className="my-4" />
-            <div className="flex flex-row gap-8 pb-4">
+            <div className="flex flex-row gap-8">
               <Button color="primary" onPress={() => onOpenAddMovie()}>
                 <Film />
                 Додати фільм
