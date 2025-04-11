f23df029df6e50179f8df4edcf328b574a4fe88d
k5sha <gositer2003@gmail.com>
2025-04-11 15:48:49 +0300

    fix: api.go

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -119,7 +119,7 @@ func (app *application) mount() *chi.Mux {
 		r.Get("/health", app.healthCheckHandler)
 
 		// Swagger
-		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
+		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.apiURL)
 		r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(docsURL)))
 
 		r.Route("/rooms", func(r chi.Router) {
