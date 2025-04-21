91bd21c93d9bc912b184056db0b77e726cb11371
Yurii <yurri.yevtushenko.work@gmail.com>
2025-04-21 18:02:58 +0300

    fix: docker file web

--- web/src/config/site.ts
+++ web/src/config/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: import.meta.env.VITE_APP_NAME || "Ticketo",
-  server_api: import.meta.env.VITE_API_URL || "http://185.126.115.87/api/v1",
+  server_api: import.meta.env.VITE_API_URL || "https://k5sha.run.place/api/v1",
 };
