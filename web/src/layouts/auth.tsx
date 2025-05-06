e98617481438a7712f70475a0c77bbee56150bf8
Yurii Yevtushenko <gositer2003@gmail.com>
2025-05-06 12:35:20 +0300

    feat: add logo + back on auth pages

--- web/src/layouts/auth.tsx
+++ web/src/layouts/auth.tsx
@@ -1,9 +1,19 @@
+import { Link } from "@heroui/link";
 import { ReactNode } from "react";
+import { ChevronLeft } from 'lucide-react';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+      <Link className="" href="/">
+        <ChevronLeft size={32}/>
+      </Link>
+        <div className="text-center py-4">
+          <Link className="font-bold text-3xl text-inherit" href="/">
+            Ticketo
+          </Link>
+        </div>
         {children}
       </div>
     </div>
