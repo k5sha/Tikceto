98670543850bf13d02503942c10a7d177020cf18
Yurii Yevtushenko <gositer2003@gmail.com>
2025-05-29 11:40:37 +0300

    fix: broken imports

--- web/src/layouts/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout.tsx
@@ -1,5 +1,5 @@
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/Navbar.tsx";
+import Footer from "../components/Footer.tsx";
 export default function DefaultLayout({
   children,
 }: {
