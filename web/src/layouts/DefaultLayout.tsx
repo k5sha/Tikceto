c6ffccb04f5294ffcce2cc2e7acfd8283dd72338
Yurii Yevtushenko <gositer2003@gmail.com>
2025-05-29 11:11:11 +0300

    fix: broken import of Navbar

--- web/src/layouts/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from '@/components/Navbar';
+import { Footer } from '@/components/Footer';
 
 export default function DefaultLayout({
   children,
@@ -11,15 +12,7 @@ export default function DefaultLayout({
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-12">
         {children}
       </main>
-      <footer className="w-full flex flex-col items-center justify-center py-6 text-gray-400 text-sm select-none border-t border-gray-100 bg-white">
-        <p className="text-sm sm:text-base font-light">
-          Created with <span className="text-red-500">♥</span> by Yurii
-          Yevtushenko
-        </p>
-        <p className="mt-1 text-xs sm:text-sm text-gray-400">
-          &copy; {new Date().getFullYear()} All rights reserved.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
