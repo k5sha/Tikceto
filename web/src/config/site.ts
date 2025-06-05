2e02864b3b7244ba6b59eb836616bc2de821ed63
Yurii Yevtushenko <gositer2003@gmail.com>
2025-06-05 18:40:19 +0300

    feat: use Traefik

--- web/src/config/site.ts
+++ web/src/config/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: import.meta.env.VITE_APP_NAME || "Ticketo",
-  server_api: import.meta.env.VITE_API_URL || "https://k5sha.run.place/api/v1",
+  server_api: import.meta.env.VITE_API_URL || "https://api.k5sha.run.place/v1",
 };
