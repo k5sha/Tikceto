59ed1190f93ce3c177ee38b0b39ca36f11879c99
Yurii Yevtushenko <gositer2003@gmail.com>
2025-07-31 11:38:37 +0300

    feat: move web part to other repo

--- web/src/config/site.ts
+++
@@ -1,6 +0,0 @@
-export type SiteConfig = typeof siteConfig;
-
-export const siteConfig = {
-  name: import.meta.env.VITE_APP_NAME || "Ticketo",
-  server_api: import.meta.env.VITE_API_URL || "https://api.tikceto.k5sha.xyz/v1",
-};
